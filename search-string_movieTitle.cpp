9037e40d2977a414e7f6b53df75ae7c23874d41b
Isaac <isaac.r.watts@gmail.com>
2020-06-24 20:00:03 -0600

    more exercise files

---
+++ search-string_movieTitle.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+   char movieTitle[100] = "The Lion King";
+   char* movieResult = nullptr;
+
+   /* Your solution goes here  */
+   movieResult = strstr(movieTitle, "The");
+
+   cout << "Movie title contains The? ";
+   if (movieResult != nullptr) {
+      cout << "Yes." << endl;
+   }
+   else {
+      cout << "No." << endl;
+   }
+
+   return 0;
+}
